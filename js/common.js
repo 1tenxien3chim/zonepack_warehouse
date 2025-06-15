fd01e484f976ae1d1a788facd1e658fa1026fac5
Dat <dyoutobe1001@gmail.com>
2025-06-15 16:18:17 +0700

    root
    root

---
+++ js/common.js
@@ -0,0 +1,139 @@
+// menu toggle
+$(function () {
+    var html = $('html, body'),
+        navContainer = $('.nav-container'),
+        navToggle = $('.nav-toggle'),
+        navDropdownToggle = $('.has-dropdown');
+    overlay = $("<div class='overlay'></div> ");
+    overlay2 = $("<div class='overlay'></div> ");
+
+    // Nav toggle
+    navToggle.on('click', function (e) {
+        overlay.toggle();
+        var $this = $(this);
+        e.preventDefault();
+        $this.toggleClass('is-active');
+        navContainer.toggleClass('is-visible');
+        html.toggleClass('nav-open');
+    });
+
+
+    $("body").prepend(overlay);
+    overlay.click(function () {
+        navToggle.trigger('click');
+        // $(this).toggle();
+    })
+
+    $("body").prepend(overlay2);
+    overlay2.click(function () {
+        $(this).toggle();
+    })
+    // Nav dropdown toggle
+    navDropdownToggle.on('click', function () {
+        var $this = $(this);
+        $this.toggleClass('is-active').siblings().removeClass('is-active'); 
+        if ($this.children('ul').hasClass('open-nav')) {
+            $this.children('ul').removeClass('open-nav');
+            $this.children('ul').slideUp(350);
+        }
+        else {
+            $this.parent().parent().find('li .nav-dropdown').removeClass('open-nav');
+            $this.parent().parent().find('li .nav-dropdown').slideUp(350);
+            $this.children('ul').toggleClass('open-nav');
+            $this.children('ul').slideToggle(350);
+        }
+    });
+
+    // Prevent click events from firing on children of navDropdownToggle
+    navDropdownToggle.on('click', '*', function (e) {
+        e.stopPropagation();
+    });
+
+
+});
+
+//scroll to top button
+// ----------- croll --------------//
+(function ($) {
+    //Scroll to Top
+    function headerStyle() {
+        if ($('.header').length) {
+            var windowpos = $(window).scrollTop();
+            var scrollLink = $('.scroll-top');
+            if (windowpos >= 185) {
+                scrollLink.addClass('open');
+            } else {
+                scrollLink.removeClass('open');
+            }
+        }
+    }
+    headerStyle();
+    // Scroll to Target
+    if ($('.scroll-to-target').length) {
+        $(".scroll-to-target").on('click', function () {
+            var target = $(this).attr('data-target');
+            // animate
+            $('html, body').animate({
+                scrollTop: $(target).offset().top
+            }, 1000);
+
+        });
+    }
+
+    $(window).on('scroll', function () {
+        headerStyle();
+    });
+
+
+})(window.jQuery);
+$(".has-dropdown-1 .menu-link").click(function () {
+    $(".product-dropdown").toggleClass("active");
+});
+
+// click language search
+$( ".box-search i" ).on( "click", function() {
+    $( ".search-pc" ).toggle();
+});
+//  fixed header 
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 10) {
+      $(".menu_header").addClass("fixd-header");
+    } else {
+      $(".menu_header").removeClass("fixd-header");
+    }
+});
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $(".menu_mb").addClass("fixd-header-mobile");
+    } else {
+      $(".menu_mb").removeClass("fixd-header-mobile");
+    }
+});
+
+
+new WOW().init();
+
+$(".slider").owlCarousel({
+    items: 1,
+    responsive: {
+        1200: { item: 1, },// breakpoint from 1200 up
+        992: { items: 1, },
+        768: { items: 1, },
+        480: { items: 1, },
+        0: { items: 1, }
+    },
+    rewind: false,
+    autoplay: false,
+    autoplayHoverPause: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 5000, //slide speed smooth
+    dots: true,
+    dotsEach: false,
+    loop: true,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    margin: 30,
+    //animateOut: ['fadeOut', 'zoomOut'], // default: false
+    //animateIn: ['fadeIn', 'zoomIn'], // default: false
+    center: false,
+});
